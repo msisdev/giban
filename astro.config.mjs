dcc745bfb2894b1e78f1113915746e0993bba2c3
boxcolli <dmg04158@gmail.com>
2025-02-06 08:59:27 +0900

    chore: add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config"
+import cloudflare from "@astrojs/cloudflare"
 import markdoc from "@astrojs/markdoc"
 import pagefind from "astro-pagefind"
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
+
   integrations: [
     markdoc(),
     pagefind(),
