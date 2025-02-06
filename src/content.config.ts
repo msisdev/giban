10d5c4c891574a8a9d47a5e93c26aa5dbe644902
boxcolli <dmg04158@gmail.com>
2025-02-06 09:31:33 +0900

    fix: add frontmatter - publish

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,44 +3,32 @@
 import { glob } from "astro/loaders"
 import { defineCollection, z } from "astro:content"
 
+const schema = z.object({
+	title: z.string(),
+	description: z.string().optional(),
+	date: z.date(),
+	tags: z.array(z.string()),
+	publish: z.boolean(),
+})
+
 const blog = defineCollection({
 	loader: glob({ pattern: "**/*.mdoc", base: "./src/content/blog" }),
-	schema: () => z.object({
-		title: z.string(),
-		description: z.string().optional(),
-		date: z.date(),
-		tags: z.array(z.string()),
-	}),
+	schema: () => schema,
 })
 
 const life = defineCollection({
 	loader: glob({ pattern: "**/*.mdoc", base: "./src/content/life" }),
-	schema: () => z.object({
-		title: z.string(),
-		description: z.string().optional(),
-		date: z.date(),
-		tags: z.array(z.string()),
-	}),
+	schema: () => schema,
 })
 
 const note = defineCollection({
 	loader: glob({ pattern: "**/*.mdoc", base: "./src/content/note" }),
-	schema: () => z.object({
-		title: z.string(),
-		description: z.string().optional(),
-		date: z.date(),
-		tags: z.array(z.string()),
-	}),
+	schema: () => schema,
 })
 
 const all = defineCollection({
 	loader: glob({ pattern: "**/*.mdoc", base: "./src/content" }),
-	schema: () => z.object({
-		title: z.string(),
-		description: z.string().optional(),
-		date: z.date(),
-		tags: z.array(z.string()),
-	}),
+	schema: () => schema,
 })
 
 export const collections = { all, blog, life, note }
