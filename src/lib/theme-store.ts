b2ba886be2574ab0d10b493b961e85a678638f00
boxcolli <dmg04158@gmail.com>
2025-02-09 06:00:38 +0900

    fix: persistent color mode

--- src/lib/theme-store.ts
+++ src/lib/theme-store.ts
@@ -1,16 +1,24 @@
+import storages from "@/config/storages"
 import { atom } from "nanostores"
 
 type Themes = "light" | "dark"
 
+const storagePreference = (() => {
+  const v = window.sessionStorage.getItem(storages.colorMode)
+  if (v === null || v === "light" || v === "dark") return v
+  return null
+})()
+
 const systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches
   ? "dark"
-  : "light";
+  : "light"
 
-export const $theme = atom<Themes>(systemPreference)
+export const $theme = atom<Themes>(storagePreference ?? systemPreference)
 
 export function switchTheme() {
   const newTheme = $theme.get() === "dark"
     ? "light"
     : "dark"
   $theme.set(newTheme)
+  window.sessionStorage.setItem(storages.colorMode, newTheme)
 }
