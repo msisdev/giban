61680779ff0db808eb060e6eb1e1907b921c1944
boxcolli <dmg04158@gmail.com>
2025-02-10 21:56:37 +0900

    fix: mdoc folder structure

--- markdoc.config.ts
+++ markdoc.config.ts
@@ -1,7 +1,7 @@
 import { defineMarkdocConfig, component } from '@astrojs/markdoc/config'
 import shiki from'@astrojs/markdoc/shiki'
-import * as nodes from "@/mdoc/nodes"
-import * as tags from "@/mdoc/tags"
+import * as nodes from "@/config/mdoc/nodes"
+import * as tags from "@/config/mdoc/tags"
 
 export default defineMarkdocConfig({
   nodes,
